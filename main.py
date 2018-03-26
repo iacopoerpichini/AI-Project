322e94f60576ad652cbd680d9a286abbb28cc22e
iacopoerpichini <iacopo.erpichini@hotmail.it>
2018-03-26 21:09:38 +0200

    Fix
    aggiustamento parametro

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     percentuali = [0.01, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9,0.95, 0.99]
     dataSets = ["balance-scale.csv","carClassifier.csv","agaricus-lepiota.csv"]
     posizione_target = [1,1,22]
-    profondita = [1, 3, 2]
+    profondita = [1, 3, 3]
     for j in range(0, dataSets.__len__()) :
         dataset, attributi, target = importa_dataset_csv(dataSets[j], posizione_target[j])
 
